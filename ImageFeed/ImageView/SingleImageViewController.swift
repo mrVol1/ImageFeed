6d193c8d544b3b92777d9a3dda20223ec0c4724b
Eduard <eduardkarimov.rb@gmail.com>
2023-08-14 12:45:09 +0500

    add new view

--- ImageFeed/ImageView/SingleImageViewController.swift
+++ ImageFeed/ImageView/SingleImageViewController.swift
@@ -9,17 +9,22 @@ import Foundation
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
     var image: UIImage! {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
         }
     }
-    
-    @IBOutlet var imageView: UIImageView!
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+    }
+    
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
     }
 }
