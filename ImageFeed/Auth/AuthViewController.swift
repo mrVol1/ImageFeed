da4400d18c9ee6703973b3cab69bfb3907b6025e
Eduard <eduardkarimov.rb@gmail.com>
2023-08-26 17:50:04 +0500

    add new screen for signUp Users

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Eduard Karimov on 26/08/2023.
+//
+
+import Foundation
+import UIKit
+
+final class AuthViewController: UIViewController {
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+}
---
+++ ImageFeed/WebViewViewController.swift
@@ -0,0 +1,61 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Eduard Karimov on 26/08/2023.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    var UnsplashAuthorizeURLString = ""
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: AccessKey),
+            URLQueryItem(name: "redirect_uri", value: RedirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: AccessScope)
+        ]
+        let url = urlComponents.url!
+        let request = URLRequest(url: url)
+        webView.load(request)
+        webView.navigationDelegate = self
+    }
+    
+    @IBAction func didTapBackButton(_ sender: Any?) {
+    }
+}
+
+extension WebViewViewController: WKNavigationDelegate {
+    
+    private func code(from navigationAction: WKNavigationAction) -> String? {
+        if
+            let url = navigationAction.request.url,
+            let urlComponents = URLComponents(string: url.absoluteString),
+            urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: { $0.name == "code" })
+        {
+            return codeItem.value                                          
+        } else {
+            return nil
+        }
+    }
+    
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+        if let code = code(from: navigationAction) {
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+}
