c15adbf7c234484b8b3c20101a2c533a9fd5f956
Eduard <eduardkarimov.rb@gmail.com>
2023-09-12 19:06:06 +0500

    add fix

--- ImageFeed/SplashView/SplashViewController.swift
+++ ImageFeed/SplashView/SplashViewController.swift
@@ -47,7 +47,7 @@ extension SplashViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
-        ProgressHUD.show()
+        UIBlockingProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -60,9 +60,9 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
-                ProgressHUD.dismiss()
+                UIBlockingProgressHUD.dismiss()
             case .failure:
-                ProgressHUD.dismiss()
+                UIBlockingProgressHUD.dismiss()
                 break
             }
         }
