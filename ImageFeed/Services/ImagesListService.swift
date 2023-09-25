68e7448c393221fc7a70215ec8d4826ea1cd59ff
Eduard <eduardkarimov.rb@gmail.com>
2023-09-26 02:20:01 +0500

    add fix

--- ImageFeed/Services/ImagesListService.swift
+++ ImageFeed/Services/ImagesListService.swift
@@ -13,15 +13,12 @@ final class ImagesListService {
     private var lastLoadedPage: Int?
     
     func fetchPhotosNextPage() {
-        // Создаем URL для вашего API
-        guard let url = URL(string: "https://api.example.com/photos?page=\(lastLoadedPage ?? 1)") else {
+        guard let url = PhotoListURL else {
             return
         }
         
-        // Создаем сессию URLSession
         let session = URLSession.shared
         
-        // Создаем задачу для получения данных
         let task = session.dataTask(with: url) { [weak self] (data, response, error) in
             guard let self = self else { return }
             
@@ -32,24 +29,19 @@ final class ImagesListService {
             
             if let data = data {
                 do {
-                    // Декодируем JSON-данные в массив фотографий
                     let decoder = JSONDecoder()
                     let photos = try decoder.decode([PhotoData].self, from: data)
                     
-                    // Добавляем загруженные фотографии к существующему массиву
                     self.photos.append(contentsOf: photos)
                     
-                    // Увеличиваем номер последней загруженной страницы
                     self.lastLoadedPage = self.lastLoadedPage ?? 0 + 1
                     
-                    // Здесь вы можете обновить пользовательский интерфейс
                 } catch {
                     print("Ошибка декодирования JSON: \(error.localizedDescription)")
                 }
             }
         }
         
-        // Запускаем задачу для получения данных
         task.resume()
     }
 }
